bd03bbaf083e8c903a01590305cc2910212c6225
Cakhanif <hanif@cakhanif.me>
2019-10-15 15:42:31 +0700

    add Find Two Min

---
+++ Maths/FindTwoMin.c
@@ -0,0 +1,43 @@
+void find2min(int arr[], int arr_size) 
+{ 
+    int i, first, second; 
+  
+    /* There should be atleast two elements */
+    if (arr_size < 2) 
+    { 
+        printf(" Invalid Input "); 
+        return; 
+    } 
+  
+    first = second = INT_MAX; 
+    for (i = 0; i < arr_size ; i ++) 
+    { 
+        /* If current element is smaller than first  
+           then update both first and second */
+        if (arr[i] < first) 
+        { 
+            second = first; 
+            first = arr[i]; 
+        } 
+  
+        /* If arr[i] is in between first and second  
+           then update second  */
+        else if (arr[i] < second && arr[i] != first) 
+            second = arr[i]; 
+    } 
+    if (second == INT_MAX) 
+        printf("There is no second smallest number\n"); 
+    else
+        printf("The smallest number is %d and second "
+               "Smallest number is %d\n", first, second); 
+} 
+
+
+/* Main Program */
+int main() 
+{ 
+    int arr[] = {12, 13, 1, 10, 34, 1}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    find2min(arr, n); 
+    return 0; 
+} 
